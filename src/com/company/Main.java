f387543db54db33b81929c24aed445d23c338fe1
Avdonin Nikita <48263067+StudentNikita@users.noreply.github.com>
2019-03-20 22:23:27 +0300

    Вместо скролла по пикселям, создал метод, который скролит до нужного нам обьекта. Убрал везде слипы, заменил на ожидание обьекта. При открытии новой страницы, так же добавил ожидание обьекта

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,6 +21,7 @@ import java.util.concurrent.TimeUnit;
  * @see #getTextByXpath(String) возвращает заголовок обхекта объекта, на вход подается его xPath
  * @see #fillForm(By, String) заполнение объекта текстом, который подается на вход
  * @see #todayPlusTwoWeeks() возвращает сегодняшнюю дату + 14 дней
+ * @see #scrollToObjectByXPath(String) скролим до нужного нам объекта
  */
 
 public class Main {
@@ -34,7 +35,7 @@ public class Main {
             System.out.println("Тест по селениум!");
             System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
             driver = new ChromeDriver();
-            JavascriptExecutor jse = (JavascriptExecutor) driver;
+
 
             String url = "https://www.rgs.ru/";
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -43,13 +44,17 @@ public class Main {
             driver.get(url);
             clicByXpath("//ol/li/a[contains(text(), 'Страхование')]");
             clicByXpath("//*[contains(text(), 'Выезжающим за рубеж')]");
-            jse.executeScript("scroll(0,1100)", "");
+            scrollToObjectByXPath("//a[contains(text(), 'Рассчитать')]");
             clicByXpath("//a[contains(text(), 'Рассчитать')]");
-            compareText(getTextByXpath("//div[contains(@class, 'page')]/span[@class='h1']"), "Страхование выезжающих за рубеж");
-            jse.executeScript("scroll(0,300)", "");
 
             Wait<WebDriver> wait = new WebDriverWait(driver, 10, 1000);
+            wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(@class, 'page')]/span[@class='h1']")))); // ждем, когда появится нужный нам объект
+
+            compareText(getTextByXpath("//div[contains(@class, 'page')]/span[@class='h1']"), "Страхование выезжающих за рубеж");
+            scrollToObjectByXPath("//button/*[contains(@class, 'content-title')]");
+
             wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//button/*[contains(@class, 'content-title')]")))); // ждем, когда появится нужный нам объект
+
 
             clicByXpath("//button/*[contains(@class, 'content-title')]");
             fillForm(By.xpath("//*[contains(@class, 'actual-input')]"), "шенген");
@@ -57,15 +62,16 @@ public class Main {
             clicByName("ArrivalCountryList");
             clicByXpath("//*[contains(text(), 'Испания')]");
             clicByName("ArrivalCountryList");
-            jse.executeScript("scroll(0,800)", "");
+            scrollToObjectByXPath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'control-label')]");
             clicByXpath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'control-label')]");
             fillForm(By.xpath("//*[contains(@class, 'clear')]/*[contains(@class, 'form-group margin')]/*[contains(@class, 'form-control')]"), todayPlusTwoWeeks());
             clicByXpath("//*[contains(text(), '90 дней')]");
             fillForm(By.xpath("//div[@data-fi-input-mode=\"combined\"]//div[@class=\"form-group\"]//input[@class=\"form-control\" != @disabled]"), "LERCHIK" + " PERCHIK");
             fillForm(By.xpath("//input[@data-test-name='BirthDate']"), "06." + "08." + "1994");
-            jse.executeScript("scroll(0,1000)", "");
+            scrollToObjectByXPath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class='calc-vzr-toggle-risk-group']//div[@class='toggle off toggle-rgs']");
             clicByXpath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class='calc-vzr-toggle-risk-group']//div[@class='toggle off toggle-rgs']");
-            jse.executeScript("scroll(0,1600)", "");
+            scrollToObjectByXPath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']");
+
 
             if (!driver.findElement(By.xpath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']")).isSelected())  // проверка на "галочку" у чекбокса, если галочка не стоит, то ставим
             {
@@ -75,15 +81,12 @@ public class Main {
 
             clicByXpath("//div[contains(@class, 'valid')]/button[contains(text(), 'Рассчитать')]");
 
-            try {                           // ставим паузу на 4 секунды, после команды "рассчитать"
-                Thread.sleep(4000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains (text(),'Комфорт')]"))));
+                scrollToObjectByXPath("//h1[contains (text(),'Кальку')]");
 
-            jse.executeScript("scroll(0,300)", "");
-            compareText(driver.findElement(By.xpath("//div/span[@data-bind]/span[contains(@class, 'text-bold')]")).getText(), "Многократные поездки в течение года");   //проверка на сформированные данные
-            compareText(driver.findElement(By.xpath("//span/span/strong[@data-bind=\"text: Name\"]")).getText(), "Шенген");
+
+            compareText(driver.findElement(By.xpath("//div/span[@data-bind]/span[contains(@class, 'text-bold')]")).getText(), "Многократные поездки в течение года");
+            compareText(driver.findElement(By.xpath("//span/span/strong[@data-bind='text: Name']")).getText(), "Шенген");
             compareText(driver.findElement(By.xpath("//strong[@data-bind=\"text: LastName() + ' ' + FirstName()\"]")).getText(), "LERCHIK" + " PERCHIK");
             compareText(driver.findElement(By.xpath("//strong[@data-bind=\" text: BirthDay.repr('moscowRussianDate')\"]")).getText(), "06." + "08." + "1994");
 
@@ -96,10 +99,9 @@ public class Main {
 
 
 
-
-
-
-
+        /**
+         * методы к тесту
+         */
 
     }
     public static void compareText (String actual, String expect){
@@ -148,4 +150,12 @@ public class Main {
         return driver.findElement(By.xpath(xPath)).getText();
 
     }
+
+    public static void scrollToObjectByXPath(String xPath){
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(By.xpath(xPath)));
+    }
+
+
+
+
 }
